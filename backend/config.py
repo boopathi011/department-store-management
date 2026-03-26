4357a9f3c382b6fcc56acc7544d0e6e4d6bf9b91
boopathi011 <boopathi.23aim@kongu.edu>
2026-03-26 21:10:24 +0530

    Final production hardening: removed legacy data and secured config

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ class Config:
     ORDERS_FILE = os.path.join(DB_DIR, 'orders.json')
     LOANS_FILE = os.path.join(DB_DIR, 'loans.json')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://Onlinestore:boopathi%402005@cluster0.9pnebvb.mongodb.net/')
+    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://user:password@cluster.mongodb.net/dbname')
     DATABASE_NAME = 'department_store'
     RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID', 'rzp_test_placeholder')
     RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET', 'secret_placeholder')
